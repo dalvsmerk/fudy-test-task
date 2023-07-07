25bef167f48f759401b42b3631a63a48c822da5b
Vladyslav Umerenko <space.vlad7@gmail.com>
2023-07-07 19:46:13 +0300

    Setup Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Test task API')
+    .setDescription('Test task API description')
+    .setVersion('0.1')
+    .addTag('fudy')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
